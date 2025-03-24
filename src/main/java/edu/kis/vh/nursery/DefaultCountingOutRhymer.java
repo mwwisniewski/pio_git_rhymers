d6cdf729bb5820204c5a80eb44ade6e1932df006
Michal Wisniewski <251243@edu.p.lodz.pl>
2025-03-24 13:34:32 +0100

    9. Zmiana widocznosci klas na mozliwie najmniejsza

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
     final static int MINUS_ONE =-1;
     private int[] numbers = new int[TABLE_MAX];
 
-    public int total = MINUS_ONE;
+    private int total = MINUS_ONE;
 
     public void countIn(int in) {
         if (!isFull())
